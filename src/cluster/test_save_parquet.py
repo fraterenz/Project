60fd6e88cdcac6ccb56f651234adf2277a75ed10
Francesco Terenzi <francesco.terenzi@epfl.ch>
2018-11-24 16:06:46 +0100

    test_parquet.py

--- src/cluster/test_save_parquet.py
+++ src/cluster/test_save_parquet.py
@@ -1,15 +1,29 @@
+import argparse
 from pyspark.sql import SparkSession
 
 
 def main():
+    parser = argparse\
+        .ArgumentParser(description='''Test if the parquet folders contains something meaningfull''')
+
+    parser.add_argument(
+        '-in',
+        metavar='path',
+        type=str,
+        required=True,
+        help='''path to parquet files'''
+    )
+
+    args = vars(parser.parse_args())
+
     spark = SparkSession.builder.getOrCreate()
 
     # loading the saved parquet files
-    # DATA_DIR = "/Users/francescoterenzi/ADA/Project/temp/test_main_filtered.parquet"
-    DATA_DIR = "/user/terenzi/filtered_conflicts.parquet"
+    DATA_DIR = args['in']
     columnar_data = spark.read.parquet(DATA_DIR)
     print(type(columnar_data))
     print(columnar_data.show(5))
+    print(columnar_data.count())
 
 
 if __name__ == "__main__":
